304a39d8f53ce0eedd2aaf8d912d2609210f34cf
ritullia <brigrita@gmail.com>
2023-02-15 21:36:58 +0200

    task 4 first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/* ------------------------------ TASK 4 -----------------------------------
+Parašykite JS kodą, kuris vartotojui atėjus į tinklalapį kreipsis į cars.json failą
+ir iš jo atvaizduos visus automobilių gamintojus ir pagamintus modelius. 
+Kiekvienas gamintojas turės savo atvaizdavimo "kortelę", kurioje bus 
+nurodomas gamintojas ir jo pagaminti modeliai.
+
+
+Pastaba: Informacija apie automobilį (brand) (jo kortelė) turi turėti 
+bent minimalų stilių;
+-------------------------------------------------------------------------- */
