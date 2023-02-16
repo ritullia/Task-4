d61af9f96b5b755f8143566bd0ccfcb1587147ca
Rita <94860684+ritullia@users.noreply.github.com>
2023-02-17 01:50:19 +0200

    Dev (#1)
    * data from fetch commit
    
    * cars card commit
    
    * style & function correction commit
    
    * style commit
    
    * function & style commit

--- script.js
+++ script.js
@@ -8,3 +8,69 @@ nurodomas gamintojas ir jo pagaminti modeliai.
 Pastaba: Informacija apie automobilį (brand) (jo kortelė) turi turėti 
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
+console.log("veikia")
+
+
+const carsData = './cars.json'
+
+const mainCardDiv = document.getElementById
+
+let data;
+
+async function getData() {
+    fetch(carsData)
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+            data = createCard(result)
+
+        })
+        .catch((error) => console.error(error));
+
+}
+
+
+function createCard(data) {
+    // console.log(data)
+
+
+    let cardsContainer = document.getElementById('output');
+    const cardBody = document.createElement('div');
+    cardBody.classList.add("cards-container")
+
+    data.forEach((key) => {
+        // console.log(key)
+        const carDiv = document.createElement('div')
+        carDiv.classList.add("cars-card")
+
+        const carBrand = document.createElement('h3')
+        carBrand.textContent = key.brand
+
+        const carModelsDiv = document.createElement('div')
+
+        const carModels = document.createElement('p');
+        carModels.classList.add("models-text")
+        carModels.textContent = key.models.toString()
+
+        carModelsDiv.append(carModels)
+        carDiv.append(carBrand, carModelsDiv)
+        cardBody.append(carDiv)
+
+    });
+
+    cardsContainer.append(cardBody)
+    return cardsContainer
+}
+
+
+
+
+
+getData()
+
+
+
+
+
+
+
